29389b1fc861fdc324597f45fff7c583025cfd1a
hemuvyas <58726792+hemuvyas@users.noreply.github.com>
2020-04-29 19:38:20 +0530

    Date: 29 April 2020 - Linking of library file, to start with javascript.

--- trial/src/Plugin/Block/MovieBlock.php
+++ trial/src/Plugin/Block/MovieBlock.php
@@ -24,65 +24,73 @@ class MovieBlock extends BlockBase {
     $page_title = $node_details->title->value;
     $page_title = 'Movies of '. $page_title;
     $query = \Drupal::entityQuery('node')
-      ->condition('type', 'movie')
-      ->condition('field_actor_role.entity:paragraph.field_actor.target_id',$nid)
-      ->sort('field_release_date', 'DESC');
+    ->condition('type', 'movie')
+    ->condition('field_actor_role.entity:paragraph.field_actor.target_id',$nid)
+    ->sort('field_release_date', 'DESC');
     $mids = $query->execute();
     if(empty($mids)) {
       $data = array("#markup" => "No Results Found");
     }
     else {
       $nodes = entity_load_multiple('node', $mids);
-      // kint($mids);
+      // kint($nodes);
       $items = array();
       $actor = array();
       foreach($nodes as $node) {
+        $ratings = $node->field_movie_rating->getValue();
+        $rating = $ratings[0]['rating'];
+        $rating = $rating/20;
+        $rating = $rating."/"."5";
         $mid = $node->id();
         $node_title = $node->title->value;
+        $node_des = $node->body->value;
         $node_image_fid = $node->get('field_poster')->target_id;
         $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
         $image_entity_url = $image_entity->url();
         $target_id = $node->field_actor_role->getValue();
+        // kint($target_id);
         $actors = array();
         $count = 0;
         $coactors = array ();
         foreach ($target_id as $value) {
           $paragraph = Paragraph::load($value['target_id']);
+          // kint($paragraph);
           $actor_id = $paragraph->field_actor->target_id;
           $actor = Node::load($actor_id);
-          $actors[$count]['name'] = $actor->title->value;
-          $actors[$count]['nid'] = $actor->nid->value;
+          // $actors[$count]['name'] = $actor->title->value;
+          // $actors[$count]['nid'] = $actor->nid->value;
           if ($actor_id != $nid ) {
             $coactors[$count]['name'] = $actor->title->value;
             $coactors[$count]['nid'] = $actor->nid->value;
+          }
+          else {
+            $role = $paragraph->field_role->getValue();
+            $actor_role = $role[0]['value'];
           }
           $count++;
         }
 
         $items[] = [
           'name' => $node_title,
-          'nid' => $node_id,
+          'nid' => $mid,
           'des' => $node_des,
-          'poster' => $node_poster,
-          'actors' =>$actors,
+          'poster' => $image_entity_url,
+          'role' =>$actor_role,
           'ratings' =>$rating,
           'costars' => $coactors
         ];
       }
     }
-    kint($items);
+    // kint($items);
 
 
-    // return [
-    //   '#markup' => $this->t('Hello, World! ' . $nid),
-    //   '#cache' => [
-    //     'max-age' => 0,
-    //   ],
-    // ];
+    return [
+      '#theme' => 'movie_block_actor',
+      '#items' => $items,
+      '#title' => $page_title,
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
   }
 }
-
-
-
-
-
