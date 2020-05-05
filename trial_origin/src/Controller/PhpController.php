b134913b7dbae9f6cf250d58c36066dd9176d79f
hemuvyas <58726792+hemuvyas@users.noreply.github.com>
2020-05-05 18:55:22 +0530

    Date: 5 May 2020 -> Displayed the average rating correctly instead of most recent.

--- trial_origin/src/Controller/PhpController.php
+++ trial_origin/src/Controller/PhpController.php
@@ -101,79 +101,86 @@ class PhpController
     else {
       $nodes = entity_load_multiple('node', $nids);
       foreach($nodes as $node){
+        $mid = $node->id();
         $title = $node->title->value;
         $body = $node->get('body')->value;
         $date = $node->get('field_release_date')->value;
-        $ratings = $node->field_movie_rating->getValue();
-        $rating = $ratings[0]['rating'];
-        $rating = $rating/20;
-        $rating = $rating."/"."5";
-        $node_image_fid = $node->get('field_poster')->target_id;
-        $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
-        $image_entity_url = $image_entity->url();
-        $para = $node->field_actor_role->getValue();
-        $actors = array();
-        $count = 0;
-        foreach ($para as $value) {
-          $paragraph = Paragraph::load($value['target_id']);
-          $actor_id = $paragraph->field_actor->target_id;
-          $actor = Node::load($actor_id);
-          $actors[$count]['name'] = $actor->title->value;
-          $actors[$count]['urls'] = '/node/'.$actor_id;
-          $count++;
-        }
-        $url = array();
-        $items[] = [
-          'name' => $title,
-          'url' => $image_entity_url,
-          'desc' => $body,
-          'date' => $date,
-          'rating' => $rating,
-          'cast' => $actors,
-        ];
+        $database = \Drupal::database();
+        $query = $database->query("SELECT value FROM {votingapi_result}
+          where function = 'vote_average' and entity_id = $mid");
+        $result = $query->fetchAll();
+        $rating = $result[0]->value/20;
+        $halfStarFlag = false;
+        if($result[0]->value%20 !=0) {
+         $halfStarFlag = true;
+       }
+       $node_image_fid = $node->get('field_poster')->target_id;
+       $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
+       $image_entity_url = $image_entity->url();
+       $para = $node->field_actor_role->getValue();
+       $actors = array();
+       $count = 0;
+       foreach ($para as $value) {
+        $paragraph = Paragraph::load($value['target_id']);
+        $actor_id = $paragraph->field_actor->target_id;
+        $actor = Node::load($actor_id);
+        $actors[$count]['name'] = $actor->title->value;
+        $actors[$count]['urls'] = '/node/'.$actor_id;
+        $count++;
       }
+      $url = array();
+      $items[] = [
+        'name' => $title,
+        'url' => $image_entity_url,
+        'desc' => $body,
+        'date' => $date,
+        'rating' => $rating,
+        'cast' => $actors,
+        'halfStarFlag' => $halfStarFlag,
+      ];
     }
-    $count=0;
-    foreach($nids as $nid){
-      $items[$count]['url_movie'] = "/node/".$nid;
-      $count = $count+1;
-    }
-    $items['form'] = $form_rendered;
+  }
+  $count=0;
+  foreach($nids as $nid){
+    $items[$count]['url_movie'] = "/node/".$nid;
+    $count = $count+1;
+  }
+  $items['form'] = $form_rendered;
     // kint($items['form']);
-    return array(
-      '#theme' => 'movie_list',
-      '#items' => $items,
-      '#title' => 'movie database',
-    );
+  return array(
+    '#theme' => 'movie_list',
+    '#items' => $items,
+    '#title' => 'movie database',
+  );
     // return $items;
     // return "hello world";
-  }
+}
 
-  public function costar($movie=NULL, $nid=NULL)
-  {
-    $node = Node::load($movie);
-    $target_id = array();
-    $target_id = $node->field_actor_role->getValue();
-    foreach ($target_id as $value) {
-      $paragraph = Paragraph::load($value['target_id']);
-      $actor_id = $paragraph->field_actor->target_id;
-      if ($actor_id == $nid) {
-        $role = $paragraph->field_role->value;
-        $actor = Node::load($actor_id);
+public function costar($movie=NULL, $nid=NULL)
+{
+  $node = Node::load($movie);
+  $target_id = array();
+  $target_id = $node->field_actor_role->getValue();
+  foreach ($target_id as $value) {
+    $paragraph = Paragraph::load($value['target_id']);
+    $actor_id = $paragraph->field_actor->target_id;
+    if ($actor_id == $nid) {
+      $role = $paragraph->field_role->value;
+      $actor = Node::load($actor_id);
         // kint($actor);
-        $node_image_fid = $actor->field_actor_image->target_id;
-        $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
-        $image_entity_url = $image_entity->url();
-        $node_title = $actor->title->value;
-      }
+      $node_image_fid = $actor->field_actor_image->target_id;
+      $image_entity = \Drupal\file\Entity\File::load($node_image_fid);
+      $image_entity_url = $image_entity->url();
+      $node_title = $actor->title->value;
     }
-    $items = [
-      'name' => $node_title,
-      'image' => $image_entity_url,
-      'role' => $role,
-    ];
-    // $resp = json_encode($items);
-    return new JsonResponse($items);
   }
+  $items = [
+    'name' => $node_title,
+    'image' => $image_entity_url,
+    'role' => $role,
+  ];
+    // $resp = json_encode($items);
+  return new JsonResponse($items);
+}
 
 }
